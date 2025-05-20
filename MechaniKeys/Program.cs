70503e1e0b9a42bd1ae47ab3fb582558b90c47ca
Morphox <tom@dlnet.us>
2025-05-20 16:51:53 +0200

    improvements
    makes life easier with linear switch keyboard (real story)

--- MechaniKeys/Program.cs
+++ MechaniKeys/Program.cs
@@ -12,7 +12,6 @@ namespace MechaniKeys
     internal static class Program
     {
         static SoundPlayer _player = new SoundPlayer();
-        static Random _rand = new Random();
 
         static int _volume = 30;
 
@@ -35,10 +34,7 @@ namespace MechaniKeys
 
                 PlaySound(e.Key, e.Character, e.IsSysKey);
             };
-            listener.KeyUp += (_, e) =>
-            {
-                down.Remove(e.Key);
-            };
+            listener.KeyUp += (_, e) => { down.Remove(e.Key); };
 
             new Thread(() =>
             {
@@ -59,42 +55,63 @@ namespace MechaniKeys
                     }
                     catch
                     {
-
                     }
 
                     Thread.Sleep(1000);
                 }
             }).Start();
 
-            Application.ThreadExit += (_, __) =>
-            {
-                listener.Dispose();
-            };
+            Application.ThreadExit += (_, __) => { listener.Dispose(); };
             Application.Run();
         }
 
         static void PlaySound(Key key, string str, bool sys)
         {
             var vol = _volume / 100f;
+            var pitch = 1f;
 
+            var caps = str.Length == 1 && str.ToLower() != str;
+            if (caps)
+                vol += 0.4f;
+            
             switch (key)
             {
                 case Key.Back:
-                    _player.Play("backspace", vol);
+                    _player.Play("backspace", vol, pitch);
                     break;
                 case Key.Space:
-                    _player.Play("space", vol);
+                    _player.Play("space", vol, pitch);
                     break;
                 case Key.Enter:
-                    _player.Play("enter", vol);
+                    _player.Play("enter", vol + 0.35f, pitch);
+                    break;
+                case Key.Insert:
+                case Key.Capital:
+                    _player.Play("space", vol + 0.35f, pitch);
+                    break;
+                case Key.Delete:
+                    _player.Play("backspace", vol + 0.35f, pitch);
+                    break;
+                case Key.RightAlt:
+                    _player.PlayKeyTap(vol + 0.35f, pitch);
+                    break;
+                case Key.LeftAlt:
+                case Key.LeftCtrl:
+                case Key.RightCtrl:
+                case Key.RightShift:
+                case Key.LeftShift:
+                case Key.Up:
+                case Key.Down:
+                case Key.Left:
+                case Key.Right:
+                case Key.PageUp:
+                case Key.PageDown:
+                    _player.PlayKeyTap(vol, pitch);
                     break;
                 default:
                     if (str.Length != 1 || sys)
                         return;
-
-                    var id = _player.KeyTaps[_rand.Next(_player.KeyTaps.Count)];
-
-                    _player.Play(id, vol);
+                    _player.PlayKeyTap(vol, pitch);
                     break;
             }
         }
--- MechaniKeys/SoundPlayer.cs
+++ MechaniKeys/SoundPlayer.cs
@@ -8,58 +8,67 @@ using Un4seen.Bass.AddOn.Fx;
 
 namespace MechaniKeys
 {
-	class SoundPlayer : IDisposable
-	{
-		private readonly Dictionary<string, string> _sounds = new Dictionary<string, string>();
-		public List<string> KeyTaps = new List<string>();
+    class SoundPlayer : IDisposable
+    {
+        private readonly Dictionary<string, string> _sounds = new Dictionary<string, string>();
 
-		public SoundPlayer()
-		{
-			Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+        private readonly Random _rand = new Random();
 
-			Directory.CreateDirectory("assets/sounds/key");
+        public List<string> KeyTaps = new List<string>();
 
-			var keys = Directory.GetFiles("assets/sounds/key");
+        public SoundPlayer()
+        {
+            Bass.BASS_Free();
+            Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+
+            Directory.CreateDirectory("assets/sounds/key");
+
+            var keys = Directory.GetFiles("assets/sounds/key");
             foreach (var file in keys)
             {
-				var id = $"key/{Path.GetFileNameWithoutExtension(file)}";
+                var id = $"key/{Path.GetFileNameWithoutExtension(file)}";
 
-				Cache(id);
+                Cache(id);
 
-				KeyTaps.Add(id);
-			}
-
-			var specific = Directory.GetFiles("assets/sounds");
-			foreach (var file in specific)
-            {
-				Cache(Path.GetFileNameWithoutExtension(file));
+                KeyTaps.Add(id);
             }
-		}
 
-		public void Cache(string id, string ext = "wav")
-		{
-			_sounds.Add(id, $"assets/sounds/{id}.{ext}");
-		}
+            var specific = Directory.GetFiles("assets/sounds");
+            foreach (var file in specific)
+            {
+                Cache(Path.GetFileNameWithoutExtension(file));
+            }
+        }
 
-		public void Play(string id, float volume = 1, float pitch = 1)
-		{
-			if (_sounds.TryGetValue(id, out var sound))
-			{
-				var s = Bass.BASS_StreamCreateFile(sound, 0, 0, BASSFlag.BASS_STREAM_DECODE | BASSFlag.BASS_STREAM_PRESCAN | BASSFlag.BASS_FX_FREESOURCE);//sound, 0, 0, BASSFlag.BASS_STREAM_AUTOFREE);
-				
-				s = BassFx.BASS_FX_TempoCreate(s, BASSFlag.BASS_STREAM_PRESCAN | BASSFlag.BASS_STREAM_AUTOFREE);
+        public void Cache(string id, string ext = "wav")
+        {
+            _sounds.Add(id, $"assets/sounds/{id}.{ext}");
+        }
 
-				Bass.BASS_ChannelSetAttribute(s, BASSAttribute.BASS_ATTRIB_VOL, volume);
-				Bass.BASS_ChannelSetAttribute(s, BASSAttribute.BASS_ATTRIB_TEMPO_PITCH, (pitch - 1) * 60);
+        public void Play(string id, float volume = 1, float pitch = 1)
+        {
+            if (_sounds.TryGetValue(id, out var sound))
+            {
+                var s = Bass.BASS_StreamCreateFile(sound, 0, 0,
+                    BASSFlag.BASS_STREAM_DECODE | BASSFlag.BASS_STREAM_PRESCAN |
+                    BASSFlag.BASS_FX_FREESOURCE);
 
-				//Bass.BASS_ChannelPlay(sound, false);
-				Bass.BASS_ChannelPlay(s, false);
-			}
-		}
+                s = BassFx.BASS_FX_TempoCreate(s, BASSFlag.BASS_STREAM_PRESCAN | BASSFlag.BASS_STREAM_AUTOFREE);
+                Bass.BASS_ChannelSetAttribute(s, BASSAttribute.BASS_ATTRIB_VOL, volume);
 
-		public void Dispose()
-		{
-			Bass.BASS_Free();
-		}
-	}
+                //Bass.BASS_ChannelPlay(sound, false);
+                Bass.BASS_ChannelPlay(s, false);
+            }
+        }
+
+        public void PlayKeyTap(float volume = 1, float pitch = 1)
+        {
+            Play(KeyTaps[_rand.Next(KeyTaps.Count)], volume, pitch);
+        }
+
+        public void Dispose()
+        {
+            Bass.BASS_Free();
+        }
+    }
 }
